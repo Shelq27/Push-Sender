7c5a175a1513a6ecf9998d893cc82f9f82e063ef
Shelq <klcao@yandex.ru>
2023-10-12 21:32:00 +0300

    Create Push-Sender

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,46 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "LIKE")
+        .putData(
+            "content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    val message2 = Message.builder()
+        .putData("action", "NEWPOST")
+        .putData(
+            "content", """{
+          "postId": 1,
+          "postAuthor": "Ivan",
+          "postSmallContent": "Hey it test",
+          "postBigContent": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. "
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+    FirebaseMessaging.getInstance().send(message2)
+}
